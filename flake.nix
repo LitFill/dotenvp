0691f2b5b3a7a596fc727a229497b7e86834e1d9
LitFill <marrazzy54@gmail.com>
2026-09-13 03:52:39 +0700

    flake: add flake-utils and packages.default

--- flake.nix
+++ flake.nix
@@ -3,19 +3,41 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
-    { nixpkgs, ... }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-    in
     {
-      devShells.${system}.default = pkgs.mkShell {
-        packages = with pkgs; [
-          koka
-        ];
-      };
-    };
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        packages.default = pkgs.stdenv.mkDerivation {
+          pname = "dotenvp";
+          version = "0.0.1";
+          src = ./.;
+          nativeBuildInputs = with pkgs; [
+            koka
+          ];
+          buildPhase = ''
+            mkdir -p $out/bin
+            koka -o $out/bin/dotenvp main.kk
+            chmod +x $out/bin/dotenvp
+          '';
+          dontInstall = true;
+        };
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            koka
+          ];
+        };
+      }
+    );
 }
