c16877167c3d03c9514db76ff59f5fd9e1e3c1db
LitFill <marrazzy54@gmail.com>
2026-09-15 23:26:05 +0700

    chore(flake): bump version

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
       {
         packages.default = pkgs.stdenv.mkDerivation {
           pname = "dotenvp";
-          version = "0.0.3";
+          version = "0.0.4";
           src = ./.;
           nativeBuildInputs = with pkgs; [
             koka
